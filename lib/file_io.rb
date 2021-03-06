2f4b206f494e93c2c5443706b0b4757ef75079bc
brueck1988 <brueck1988@gmail.com>
2021-03-06 16:48:15 -0700

    Added file io class and test

---
+++ lib/file_io.rb
@@ -0,0 +1,16 @@
+require './lib/night_writer'
+
+class FileIo
+message_file = File.open("message.txt")
+text_to_translate = message_file.read
+#message_readlines = message_file.readlines.map(&:chomp)
+message_file.close
+print "message.txt braille.txt\nCreated 'braille.txt' containing 256 characters"
+
+text = NightWriter.new(text_to_translate)
+braille = text.convert_to_braille
+
+braille_file = File.new("braille.txt", "w")
+braille_file.puts(braille)
+braille_file.close
+end
