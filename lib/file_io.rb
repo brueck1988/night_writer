d75f40f7aa7b5682fb2c064aa199081dc05eba07
brueck1988 <brueck1988@gmail.com>
2021-03-06 18:42:45 -0700

    Updated file io class

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -1,16 +1,36 @@
 require './lib/night_writer'
 
 class FileIo
-message_file = File.open("message.txt")
-text_to_translate = message_file.read
-#message_readlines = message_file.readlines.map(&:chomp)
-message_file.close
-print "message.txt braille.txt\nCreated 'braille.txt' containing 256 characters"
 
-text = NightWriter.new(text_to_translate)
-braille = text.convert_to_braille
+  def start
+    read_file
+    count_characters_in_input
+    write_file
+    print_message
+  end
 
-braille_file = File.new("braille.txt", "w")
-braille_file.puts(braille)
-braille_file.close
+  def read_file
+  message_file = File.open("message.txt")
+  @text_to_translate = message_file.read
+  #message_readlines = message_file.readlines.map(&:chomp)
+  message_file.close
+  end
+
+  def write_file
+  @text = NightWriter.new(@text_to_translate)
+  braille = @text.convert_to_braille
+
+  braille_file = File.new("braille.txt", "w")
+  braille_file.puts(braille)
+  braille_file.close
+  end
+
+  def print_message
+    print "message.txt braille.txt\nCreated 'braille.txt' containing #{@character_count} characters"
+  end
+
+  def count_characters_in_input
+    require "pry";binding.pry
+    @character_count = @text_to_translate.tr("\n","").length
+  end
 end
