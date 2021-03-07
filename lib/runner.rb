5c95a03bcc447af682584a6bf837a5a87f2e39d1
brueck1988 <brueck1988@gmail.com>
2021-03-06 18:43:50 -0700

    Updated file io class

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,15 +1,4 @@
-require './lib/braille_dictionary'
-require './lib/night_writer'
+require './lib/file_io'
 
-message_file = File.open("message.txt")
-text_to_translate = message_file.read
-#message_readlines = message_file.readlines.map(&:chomp)
-message_file.close
-print "message.txt braille.txt\nCreated 'braille.txt' containing 256 characters"
-
-text = NightWriter.new(text_to_translate)
-braille = text.convert_to_braille
-
-braille_file = File.new("braille.txt", "w")
-braille_file.puts(braille)
-braille_file.close
+new_translation = FileIo.new
+new_translation.start
