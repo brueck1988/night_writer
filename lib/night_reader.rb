7a1704104d53c69880f99a09eb496cf5fdbb35c1
brueck1988 <brueck1988@gmail.com>
2021-03-08 15:58:04 -0700

    Refactored night_reader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,15 +1,24 @@
 require "./lib/alphanumeric_dictionary"
+require './lib/file_io'
 
 class NightReader
+  include FileIo
   attr_reader :text_to_convert
 
-  def initialize(text_to_convert)
-    @text_to_convert = text_to_convert
+  def initialize
+    @text_to_translate = read_text_file(ARGV[0])
     @text_message = []
   end
 
+  def start
+    count_characters_in_input
+    convert_to_text
+    write_file(ARGV[1], @text_message)
+    print_message
+  end
+
   def convert_to_text
-    split_text = @text_to_convert.split("\n")
+    split_text = @text_to_translate.split("\n")
     until split_text == []
       @braille_line = [[],[],[]]
       @text_message_line = []
@@ -28,4 +37,15 @@ class NightReader
     end
     @text_message
   end
+
+  def count_characters_in_input
+    @character_count = @text_to_translate.tr("\n","").length
+  end
+
+  def print_message
+    print "Created '#{ARGV[1]}' containing #{@character_count} characters."
+  end
 end
+
+new_translation = NightReader.new
+new_translation.start
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -12,7 +12,7 @@ class NightWriter
               :text_to_convert
 
   def initialize
-    @text_to_convert = read_text_file(ARGV[0])
+    @text_to_translate = read_text_file(ARGV[0])
     @braille_message = []
   end
 
@@ -25,7 +25,7 @@ class NightWriter
   end
 
   def make_strings_of_forty_characters_or_less
-    single_line = @text_to_convert.tr("\n","")
+    single_line = @text_to_translate.tr("\n","")
     @elements_of_twenty = []
     until single_line.empty?
       @elements_of_twenty << single_line.slice!(0..39)
@@ -49,7 +49,7 @@ class NightWriter
   end
 
   def count_characters_in_input
-    @character_count = @text_to_convert.tr("\n","").length
+    @character_count = @text_to_translate.tr("\n","").length
   end
 
   def print_message
