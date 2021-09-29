3cba47b4677d783ea5297dc79d99eb30b147726b
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-09-29 17:52:02 -0600

    Converted braille dictionary from conditionals to hash for improved O(1) performance

--- lib/braille_dictionary.rb
+++ lib/braille_dictionary.rb
@@ -6,66 +6,39 @@ class BrailleDictionary
   end
 
   def converter
-    if character == "a"
-      [["0."],[".."],[".."]]
-    elsif character == "b"
-      [["0."],["0."],[".."]]
-    elsif character == "c"
-      [["00"],[".."],[".."]]
-    elsif character == "d"
-      [["00"],[".0"],[".."]]
-    elsif character == "e"
-      [["0."],[".0"],[".."]]
-    elsif character == "f"
-      [["00"],["0."],[".."]]
-    elsif character == "g"
-      [["00"],["00"],[".."]]
-    elsif character == "h"
-      [["0."],["00"],[".."]]
-    elsif character == "i"
-      [[".0"],["0."],[".."]]
-    elsif character == "j"
-      [[".0"],["00"],[".."]]
-    elsif character == "k"
-      [["0."],[".."],["0."]]
-    elsif character == "l"
-      [["0."],["0."],["0."]]
-    elsif character == "m"
-      [["00"],[".."],["0."]]
-    elsif character == "n"
-      [["00"],[".0"],["0."]]
-    elsif character == "o"
-      [["0."],[".0"],["0."]]
-    elsif character == "p"
-      [["00"],["0."],["0."]]
-    elsif character == "q"
-      [["00"],["00"],["0."]]
-    elsif character == "r"
-      [["0."],["00"],["0."]]
-    elsif character == "s"
-      [[".0"],["0."],["0."]]
-    elsif character == "t"
-      [[".0"],["00"],["0."]]
-    elsif character == "u"
-      [["0."],[".."],["00"]]
-    elsif character == "v"
-      [["0."],["0."],["00"]]
-    elsif character == "w"
-      [[".0"],["00"],[".0"]]
-    elsif character == "x"
-      [["00"],[".."],["00"]]
-    elsif character == "y"
-      [["00"],[".0"],["00"]]
-    elsif character == "z"
-      [["0."],[".0"],["00"]]
-    elsif character == " "
-      [[".."],[".."],[".."]]
-    elsif character == "."
-      [[".."],["00"],[".0"]]
-    elsif character == ","
-      [[".."],["0."],[".."]]
-    elsif character == "'"
-      [[".."],[".."],["0."]]
+    dictionary =  {}
+    dictionary["a"] =  [["0."],[".."],[".."]]
+    dictionary["b"] =  [["0."],["0."],[".."]]
+    dictionary["c"] =  [["00"],[".."],[".."]]
+    dictionary["d"] =  [["00"],[".0"],[".."]]
+    dictionary["e"] =  [["0."],[".0"],[".."]]
+    dictionary["f"] =  [["00"],["0."],[".."]]
+    dictionary["g"] =  [["00"],["00"],[".."]]
+    dictionary["h"] =  [["0."],["00"],[".."]]
+    dictionary["i"] =  [[".0"],["0."],[".."]]
+    dictionary["j"] =  [[".0"],["00"],[".."]]
+    dictionary["k"] =  [["0."],[".."],["0."]]
+    dictionary["l"] =  [["0."],["0."],["0."]]
+    dictionary["m"] =  [["00"],[".."],["0."]]
+    dictionary["n"] =  [["00"],[".0"],["0."]]
+    dictionary["o"] =  [["0."],[".0"],["0."]]
+    dictionary["p"] =  [["00"],["0."],["0."]]
+    dictionary["q"] =  [["00"],["00"],["0."]]
+    dictionary["r"] =  [["0."],["00"],["0."]]
+    dictionary["s"] =  [[".0"],["0."],["0."]]
+    dictionary["t"] =  [[".0"],["00"],["0."]]
+    dictionary["u"] =  [["0."],[".."],["00"]]
+    dictionary["v"] =  [["0."],["0."],["00"]]
+    dictionary["w"] =  [[".0"],["00"],[".0"]]
+    dictionary["x"] =  [["00"],[".."],["00"]]
+    dictionary["y"] =  [["00"],[".0"],["00"]]
+    dictionary["z"] =  [["0."],[".0"],["00"]]
+    dictionary[" "] =  [[".."],[".."],[".."]]
+    dictionary["."] =  [[".."],["00"],[".0"]]
+    dictionary[","] =  [[".."],["0."],[".."]]
+    dictionary["'"] =  [[".."],[".."],["0."]]
+    if dictionary[@character]
+      dictionary[@character]
     else
       "Undefined Character"
     end
