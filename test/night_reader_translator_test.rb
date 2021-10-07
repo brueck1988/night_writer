21e1772e6d149db4581e8c84722dace3ba30bff5
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-07 14:10:01 -0600

    Update tests per refactor

--- test/night_reader_translator_test.rb
+++ test/night_reader_translator_test.rb
@@ -5,28 +5,28 @@ require './lib/night_reader_translator'
 
 class NightReaderTranslatorTest < Minitest::Test
   def test_it_exists
-    night_reader_translator = NightReaderTranslator.new([["0."],[".."],[".."]])
+    night_reader_translator = NightReaderTranslator.new(["0.","..",".."])
 
     assert_instance_of NightReaderTranslator, night_reader_translator
   end
 
   def test_it_has_attributes
-    night_reader_translator = NightReaderTranslator.new([["0."],[".."],[".."]])
-    expected = [["0."],[".."],[".."]]
+    night_reader_translator = NightReaderTranslator.new(["0.","..",".."])
+    expected = ["0.","..",".."]
     assert_equal expected, night_reader_translator.braille_to_translate
-    assert_equal [], night_reader_translator.text_message
+    assert_instance_of AlphanumericDictionary, night_reader_translator.dictionary
   end
 
   def test_translate_to_text
-    input = "0.0.0000\n..0....0\n........"
-    night_reader_translator = NightReaderTranslator.new(input)
+    braille_to_translate = "0.0.0000\n..0....0\n........"
+    night_reader_translator = NightReaderTranslator.new(braille_to_translate)
 
     assert_equal ["abcd"], night_reader_translator.translate_to_text
   end
 
   def test_split_braille_at_line_breaks
-    input = "0.0.0000\n..0....0\n........"
-    night_writer = NightReaderTranslator.new(input)
+    braille_to_translate = "0.0.0000\n..0....0\n........"
+    night_writer = NightReaderTranslator.new(braille_to_translate)
     expected = ["0.0.0000", "..0....0", "........"]
 
     assert_equal expected, night_writer.split_braille_at_line_breaks
