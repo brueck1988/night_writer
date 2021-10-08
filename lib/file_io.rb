68e13109bc6887a7b96d679692d95f8b6310d6b8
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-07 18:45:11 -0600

    Remove superflous intance variable

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -1,9 +1,9 @@
 module FileIo
   def read_text_file(file_name)
-  message_file = File.open(file_name)
-  @characters_to_translate = message_file.read
-  message_file.close
-  @characters_to_translate
+    message_file = File.open(file_name)
+    characters_to_translate = message_file.read
+    message_file.close
+    characters_to_translate
   end
 
   def write_file(file_name, file_contents)
